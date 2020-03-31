67a3c12e1f9ad76ce21e55c2a0f01d61855a6546
Harea Constantin <rhoklin@gmail.com>
2020-03-31 23:51:27 +0200

    - Publisher-Book relationship configured - Added tests for it in BootStrapData - Enabled h2 console in application.properties

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -27,14 +27,14 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Author eric = new Author("Eric", "Evans");
-        Book javaBook = new Book("Java","123456");
+        Book javaBook = new Book("Java", "123456");
         eric.getBooks().add(javaBook);
         javaBook.getAuthors().add(eric);
         authorRepository.save(eric);
         bookRepository.save(javaBook);
 
         Author max = new Author("Max", "Smith");
-        Book oop = new Book("OOP","454545");
+        Book oop = new Book("OOP", "454545");
         max.getBooks().add(oop);
         max.getBooks().add(javaBook);
         oop.getAuthors().add(max);
@@ -42,13 +42,30 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(oop);
 
         Publisher prenticeHall = new Publisher("Prentice Hall", "1st street", "2nd street", "LA", "CA", "1122");
+        prenticeHall.getBooks().add(javaBook);
+        prenticeHall.getBooks().add(oop);
         Publisher oreilly = new Publisher("Oreilly", "1st street", "2nd street", "NY", "DC", "1234");
+        oreilly.getBooks().add(oop);
         publisherRepository.save(prenticeHall);
         publisherRepository.save(oreilly);
+
+        oop.setPublisher(prenticeHall);
+        javaBook.setPublisher(oreilly);
+        bookRepository.save(javaBook);
+        bookRepository.save(oop);
         List<Publisher> phFromDB = (List<Publisher>) publisherRepository.findAll();
 
         System.out.println("Started in Bootstrap");
-        System.out.println("Number of books: [" + bookRepository.count() + "], number of authors: ["+ authorRepository.count()+"].");
-        phFromDB.forEach(System.out::println);
+        System.out.println("Number of books: [" + bookRepository.count() + "], number of authors: [" + authorRepository.count() + "].");
+        phFromDB.forEach(publisher -> {
+            System.out.println(publisher);
+//            System.out.println("Publisher " + publisher.getName() + "has [" + publisher.getBooks().size() + "] books");
+        });
+        System.out.println("Books:");
+        System.out.println(javaBook);
+        System.out.println(oop);
+        System.out.println("----------------\n\nPublishers:");
+        System.out.println(prenticeHall);
+        System.out.println(oreilly);
     }
 }
